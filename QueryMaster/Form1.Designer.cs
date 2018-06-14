36676b475ce4e501066fc1fd79187788ac4d4c44
ADarkHero <maik.riedlsperger@gmail.com>
2018-06-14 11:13:15 +0200

    Initial commit

--- QueryMaster/Form1.Designer.cs
+++ QueryMaster/Form1.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sqlListView = new System.Windows.Forms.ListView();
+            this.sqlQueryName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSeperator = new System.Windows.Forms.TextBox();
+            this.checkBoxAddHeadline = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // sqlListView
+            // 
+            this.sqlListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.sqlQueryName});
+            this.sqlListView.Location = new System.Drawing.Point(12, 12);
+            this.sqlListView.Name = "sqlListView";
+            this.sqlListView.Size = new System.Drawing.Size(680, 395);
+            this.sqlListView.TabIndex = 0;
+            this.sqlListView.UseCompatibleStateImageBehavior = false;
+            this.sqlListView.View = System.Windows.Forms.View.Tile;
+            this.sqlListView.DoubleClick += new System.EventHandler(this.sqlListView_DoubleClick);
+            // 
+            // sqlQueryName
+            // 
+            this.sqlQueryName.Width = 720;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 417);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seperator";
+            // 
+            // textBoxSeperator
+            // 
+            this.textBoxSeperator.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSeperator.Location = new System.Drawing.Point(12, 413);
+            this.textBoxSeperator.Name = "textBoxSeperator";
+            this.textBoxSeperator.Size = new System.Drawing.Size(13, 20);
+            this.textBoxSeperator.TabIndex = 3;
+            this.textBoxSeperator.Text = ";";
+            this.textBoxSeperator.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBoxSeperator.TextChanged += new System.EventHandler(this.textBoxSeperator_TextChanged);
+            // 
+            // checkBoxAddHeadline
+            // 
+            this.checkBoxAddHeadline.AutoSize = true;
+            this.checkBoxAddHeadline.Checked = true;
+            this.checkBoxAddHeadline.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxAddHeadline.Location = new System.Drawing.Point(121, 415);
+            this.checkBoxAddHeadline.Name = "checkBoxAddHeadline";
+            this.checkBoxAddHeadline.Size = new System.Drawing.Size(116, 17);
+            this.checkBoxAddHeadline.TabIndex = 4;
+            this.checkBoxAddHeadline.Text = "Add headline to file";
+            this.checkBoxAddHeadline.UseVisualStyleBackColor = true;
+            this.checkBoxAddHeadline.CheckedChanged += new System.EventHandler(this.checkBoxAddHeadline_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.checkBoxAddHeadline);
+            this.Controls.Add(this.textBoxSeperator);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sqlListView);
+            this.Name = "Form1";
+            this.Text = "Query Master";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView sqlListView;
+        private System.Windows.Forms.ColumnHeader sqlQueryName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSeperator;
+        private System.Windows.Forms.CheckBox checkBoxAddHeadline;
     }
 }
 
--- QueryMaster/Form1.cs
+++ QueryMaster/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Odbc;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,132 @@ namespace QueryMaster
 {
     public partial class Form1 : Form
     {
+        string sqlfolder = "Z:\\Automatisierung\\sql";
+        string seperator = ";";
+        Boolean addHeadline = true;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] sqlqueries = Directory.GetFiles(sqlfolder);
+
+            foreach(string sql in sqlqueries)
+            {
+                string filename = Path.GetFileName(sql);
+                sqlListView.Items.Add(filename);
+            }
+        }
+
+        private void sqlListView_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                string sqlFileName = sqlListView.SelectedItems[0].SubItems[0].Text;
+                executeQuery(sqlFileName);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
+        }
+
+        private void executeQuery(string sqlFileName)
+        {
+            try
+            {
+                OdbcDataReader dr = ConnectToSQLDatabase(sqlFileName);
+
+                int drlenght = dr.FieldCount;
+                string csv = "";
+                if (addHeadline)
+                {
+                    csv = readSQLHeadline(dr, drlenght, csv);
+                }
+
+                while (dr.Read())
+                {
+                    csv = readSQLLine(dr, drlenght, csv);
+                }
+
+                saveQueryToCSV(csv, sqlFileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+           
+        }
+
+        private OdbcDataReader ConnectToSQLDatabase(string sqlFileName)
+        {
+            string path = @sqlfolder + "\\" + sqlFileName;
+
+            string connectionString = "DSN=eNVenta SQL Server;Server=server-03;Database=LOE01;User Id=s.ewert;Password=loechel123;";
+            string sql = File.ReadAllText(path);
+            OdbcConnection conn = new OdbcConnection(connectionString);
+            conn.Open();
+            OdbcCommand comm = new OdbcCommand(sql, conn);
+            comm.CommandTimeout = 300; //300 seconds timeout
+            OdbcDataReader dr = comm.ExecuteReader();
+            return dr;
+        }
+
+        private string readSQLLine(OdbcDataReader dr, int drlenght, string csv)
+        {
+            //Read sql lines from database
+            for (int i = 0; i < drlenght; i++)
+            {
+                csv += dr[i].ToString() + seperator;
+            }
+            csv = csv.Remove(csv.Length - 1); //Cut last seperator
+            csv += "\r\n"; //Add newline
+            return csv;
+        }
+
+        private string readSQLHeadline(OdbcDataReader dr, int drlenght, string csv)
+        {
+            for (int i = 0; i < drlenght; i++)
+            {
+                csv += dr.GetName(i) + seperator;
+            }
+            csv = csv.Remove(csv.Length - 1); //Cut last seperator
+            csv += "\r\n"; //Add newline
+            return csv;
+        }
+
+        private static void saveQueryToCSV(string csv, string sqlFileName)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "csv files (*.csv)|*.csv";
+            saveFileDialog1.FileName = DateTime.Now.ToString("ddMMyyyy-HHmm") + sqlFileName.Remove(sqlFileName.Length - 4);
+
+            //Save csv file
+            Stream savePathCheck;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((savePathCheck = saveFileDialog1.OpenFile()) != null)
+                {
+                    savePathCheck.Close();
+                    //Write all text to selected file
+                    String savePath = Path.GetFullPath(saveFileDialog1.FileName);
+                    File.WriteAllText(savePath, csv);
+                }
+            }
+        }
+
+        private void checkBoxAddHeadline_CheckedChanged(object sender, EventArgs e)
+        {
+            addHeadline = checkBoxAddHeadline.Checked;
+        }
+
+        private void textBoxSeperator_TextChanged(object sender, EventArgs e)
+        {
+            seperator = textBoxSeperator.Text;
         }
     }
 }
